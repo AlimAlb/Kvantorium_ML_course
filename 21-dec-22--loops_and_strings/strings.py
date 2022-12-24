e0fa665729da53239be4ee9d88d61565787a35e0
Alim Albogachiev <alimalbogachiev@yandex.ru>
2022-12-24 15:10:33 +0300

    28-dec class

---
+++ 21-dec-22--loops_and_strings/strings.py
@@ -0,0 +1,14 @@
+#slicing
+s = 'Hello world!'
+# print(s[1:5])
+# print(s[:6])
+# print(s[3:])
+# print(s[:-2])
+# print(s[2:-2])
+
+#Upper/lower
+print(s.upper())
+print(s.lower())
+
+#index
+print(s.index('world'))
