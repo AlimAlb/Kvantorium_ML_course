405595a0402cc329ea618fb3078deb22f527dfc5
Alim Albogachiev <alimalbogachiev@yandex.ru>
2022-12-25 22:37:27 +0300

    loops and string changed

--- 21-dec-22--loops_and_strings/loops.py
+++ 21-dec-22--loops_and_strings/loops.py
@@ -1,5 +1,7 @@
 import math
 
+
+#range(start, stop, step)
 print(range(10))
 
 print(range(10)[2])
@@ -31,18 +33,22 @@ for i in range(20):
         summ += i
 
 #fibbonacci number until n-th
-curr = 1
-next = 1
+curr_1 = 1
+curr_2 = 1
 n = int(input())
-for i in range(n):
-    next = curr + next
+for i in range(2, n):
+    curr_2 = curr_1 + curr_2
+    curr_1 = curr_2 - curr_1
+print(curr_2)
 
 #mean of n grades for grades in 0 to 5 inputed values without list
-n = int(input())
+n = int(input('Введите количество оценок: '))
 summ = 0
 for i in range(n):
-    summ += int(input())
-mean = summ/n
+    grade = int(input())
+    summ += grade
+
+print("Средний балл равен: ", summ/n)
     
 
 
@@ -53,19 +59,22 @@ while a < 5:
     a+=1
 #we can any for loop with while:
 #fibbonacci number until n-th wih while
-curr = 1
-next = 1
+curr_1 = 1
+curr_2 = 1
 n = int(input())
 i = 0
-while i < n:
-    next = curr + next
-    i+= 1
+while i + 2 < n:
+    curr_2 = curr_1 + curr_2
+    curr_1 = curr_2 - curr_1
+    i += 1
+
+print(curr_2)
 
 #input while correct word:
 n = input()
 while n != 'exit':
-    num = int(input())
-    print(num**2)
+    print(int(n)**2)
+    n = input()
 #grades with loop and input till correct
 n = int(input())
 summ = 0
@@ -79,20 +88,24 @@ while i < n:
         i+= 1
 mean = summ/n
 #simple console menu with while loop
-inp = input()
-flag = True
-while flag:
-    if inp == 'square':
-        print('square: ', int(input())**0.5)
-    elif inp == 'quad':
-        print('quad: ', int(input())**2)
-    elif inp == 'sin':
-        print('sin: ', math.sin(int(input())))
-    elif inp == 'exit':
-        print("Exit.....")
-        flag = False
+import math
+#while exit - squared numbers
+end_flag = False
+#**2
+#sqrt
+#sin
+print("use three operations:\n1. power\n2. sqrt\n3. sin\n")
+while not(end_flag):
+    path = input('operation:')
+    if path == 'power':
+        print(int(input('number:'))**2)
+    elif path == 'sqrt':
+        print(int(input('number:'))**0.5)
+    elif path == 'sin':
+        print(math.sin(int(input('number:'))))
     else:
-        print("invalid input, try again:")
+        print('invalid operation...')
+        end_flag = True
 
 #break
 #break statement allows us stop loop under some conditionbs
@@ -113,4 +126,17 @@ while True:
 
 #continue
 #continue statement allows us to skip iteration of loop under some conditions
-#sum of 
+#sum of odd numbers:
+summ = 0
+for i in range(20):
+    if i % 2 != 0:
+        continue
+    sum += i
+
+#squares of numbers that devides by 3
+for i in range(20):
+    if i % 3 != 0:
+        continue
+    print(i**2)
+
+#_in_ key word
--- 21-dec-22--loops_and_strings/strings.py
+++ 21-dec-22--loops_and_strings/strings.py
@@ -1,10 +1,10 @@
 #slicing
 s = 'Hello world!'
-# print(s[1:5])
-# print(s[:6])
-# print(s[3:])
-# print(s[:-2])
-# print(s[2:-2])
+print(s[1:5])
+print(s[:6])
+print(s[3:])
+print(s[:-2])
+print(s[2:-2])
 
 #Upper/lower
 print(s.upper())
@@ -12,3 +12,60 @@ print(s.lower())
 
 #index
 print(s.index('world'))
+
+#isalnum, isalpha, isdigit, islower, isupper
+print('123123asd'.isalnum()) #[a-z] [0-9]
+print('123123asd'.isalpha()) #[a-z]
+print('123123asd'.isdigit()) #[0-9]
+print('asd'.islower())
+print('asd'.isupper())
+
+#Проверить,введенные данные: имя, фамилия, номер телефона
+
+#join
+print(''.join(['Hello', 'world']))
+print(' '.join(['Hello', 'world!']))
+print('++++'.join(['Hello', 'world!']))
+
+#Соединить все данные в одну строку: Name: <name>, Surname: <surname>, number: <number>
+
+#split
+print("Hello world!".split())
+print('Hello,  how are u?'.split())
+
+#Ввод данных в определенном формате и дальнейшее преобразование в формат предыдущего задания
+inp = input('Введите имя, фамилию и номер без +:')
+
+lst = inp.split()
+error_flag = False
+
+if lst[0].isalpha():
+    print('Имя введено верно...')
+else:
+    print('Ошибка в имени...')
+    error_flag = True
+
+if lst[1].isalpha():
+    print('Фамилия введена верно...')
+else:
+    print('Ошибка в фамилии...')
+    error_flag = True
+
+
+if lst[2].isdigit():
+    print('номер введен верно...')
+else:
+    print('Ошибка в номерe...')
+    error_flag = True
+
+if not(error_flag):
+    out = ' '.join(['Name: ', lst[0], ', Surname: ', lst[1], ', Number: ', lst[2]])
+    print(out)
+
+
+#Форматировнаие
+
+
+
+
+
